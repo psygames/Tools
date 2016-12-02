3098721fcf8182b95842d3ea64cc4ac56597fa58
yinlong <85171642@163.com>
2016-12-02 17:01:08 +0800

    添加XRandom-CSharp

---
+++ XRandom/XRandom-CSharp/XRandom/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RedStone.Tools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XRandom rand = new XRandom(50L);
+            for (int i = 0; i < 5; i++)
+                Console.WriteLine("Int(100): " + rand.nextInt(100));
+            for (int i = 0; i < 5; i++)
+                Console.WriteLine("Float: " + rand.nextFloat());
+            for (int i = 0; i < 5; i++)
+                Console.WriteLine("Double: " + rand.nextDouble());
+            Console.ReadKey();
+        }
+    }
+}
